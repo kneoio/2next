344513dc68cda55dd3dde1e7b8745fedd1bf3b90
kaira <justaidajam@gmail.com>
2025-05-27 19:57:17 +0100

    #

--- src/main/java/io/kneo/core/controller/AbstractSecuredController.java
+++ src/main/java/io/kneo/core/controller/AbstractSecuredController.java
@@ -3,8 +3,13 @@ package io.kneo.core.controller;
 import io.kneo.core.service.UserService;
 import io.smallrye.jwt.auth.principal.DefaultJWTCallerPrincipal;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import jakarta.validation.ConstraintViolation;
 import jakarta.ws.rs.container.ContainerRequestContext;
+
+import java.util.Set;
 
 public abstract class AbstractSecuredController<T, V> extends AbstractController<T, V> {
     protected static final String UUID_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
@@ -31,5 +36,40 @@ public abstract class AbstractSecuredController<T, V> extends AbstractController
         } else {
             rc.next();
         }
+    }
+
+    protected void handleValidationErrors(RoutingContext rc, Set<? extends ConstraintViolation<?>> violations) {
+        JsonArray errorDetails = new JsonArray();
+        for (ConstraintViolation<?> violation : violations) {
+            JsonObject errorDetail = new JsonObject()
+                    .put("field", violation.getPropertyPath().toString())
+                    .put("message", violation.getMessage());
+            errorDetails.add(errorDetail);
+        }
+        JsonObject errorResponse = new JsonObject()
+                .put("message", "Validation failed")
+                .put("errors", errorDetails);
+        rc.response()
+                .setStatusCode(400)
+                .putHeader("Content-Type", "application/json")
+                .end(errorResponse.encode());
+    }
+
+    protected void sendJsonError(RoutingContext rc, int statusCode, String errorMessage) {
+        JsonObject errorResponse = new JsonObject().put("error", errorMessage);
+        rc.response()
+                .setStatusCode(statusCode)
+                .putHeader("Content-Type", "application/json")
+                .end(errorResponse.encode());
+    }
+
+    protected void sendJsonError(RoutingContext rc, int statusCode, String errorMessage, String errorDetail) {
+        JsonObject errorResponse = new JsonObject()
+                .put("error", errorMessage)
+                .put("detail", errorDetail);
+        rc.response()
+                .setStatusCode(statusCode)
+                .putHeader("Content-Type", "application/json")
+                .end(errorResponse.encode());
     }
 }
