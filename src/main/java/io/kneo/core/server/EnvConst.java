d0d76cf1e4098121c945ecf1061ef8628ee72848
kaira <justaidajam@gmail.com>
2026-03-06 15:57:08 +0000

    #

--- src/main/java/io/kneo/core/server/EnvConst.java
+++ src/main/java/io/kneo/core/server/EnvConst.java
@@ -3,7 +3,7 @@ package io.kneo.core.server;
 public class EnvConst {
     public static final int DEFAULT_PAGE_SIZE = 20;
     public static final String APP_ID = "2next" ;
-    public static final String VERSION = "1.2.0" ;
+    public static final String VERSION = "1.2.1" ;
     public static boolean DEV_MODE = true;
 
     public static String DEFAULT_COUNTRY_OF_NUMBER_FORMAT = "pt";
