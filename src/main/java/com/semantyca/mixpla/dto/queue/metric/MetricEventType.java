d1edcfe43c98435e0027c0562d0d428a7695a601
kaira <justaidajam@gmail.com>
2026-03-17 22:57:24 +0000

    #

--- src/main/java/com/semantyca/mixpla/dto/queue/metric/MetricEventType.java
+++ src/main/java/com/semantyca/mixpla/dto/queue/metric/MetricEventType.java
@@ -1,14 +1,27 @@
 package com.semantyca.mixpla.dto.queue.metric;
 
 public enum MetricEventType {
+    @Deprecated
     SONG_SENT,
+    @Deprecated
     INTRO_GENERATED,
+    @Deprecated
     INTRO_SKIPPED,
+    @Deprecated
     SCENE_EXHAUSTED,
+    @Deprecated
     TTS_FAILED,
+    @Deprecated
     QUEUE_MESSAGE_SENT,
+    @Deprecated
     AI_TOKENS_USED,
+    @Deprecated
     MESSAGE_RECEIVED,
+
+    FATAL_ERROR,
     ERROR,
-    MIX_FED
+    COMMAND,
+    INFORMATION,
+    IMPORTANT_INFORMATION,
+    WARNING
 }
