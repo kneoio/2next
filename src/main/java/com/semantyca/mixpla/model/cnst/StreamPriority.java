5fcda4d69a2ee2504e51045ae0c92049cf6f27f5
aidazi <biosubj@gmail.com>
2026-05-07 13:17:03 +0100

    #

---
+++ src/main/java/com/semantyca/mixpla/model/cnst/StreamPriority.java
@@ -0,0 +1,25 @@
+package com.semantyca.mixpla.model.cnst;
+
+import lombok.Getter;
+
+@Getter
+public enum StreamPriority {
+    HARD_INTERRUPT(6),
+    GENTLE_INTERRUPT(7),
+    PRIORITIZED_FRONT(8),
+    PRIORITIZED(9),
+    NORMAL(10);
+
+    private final int value;
+
+    StreamPriority(int value) {
+        this.value = value;
+    }
+
+    public static StreamPriority fromValue(int value) {
+        for (StreamPriority p : values()) {
+            if (p.value == value) return p;
+        }
+        return NORMAL;
+    }
+}
