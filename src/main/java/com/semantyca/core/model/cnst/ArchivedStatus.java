656e0d8bca2b0e4b4edc33288d6eeb467d10a240
aidazi <biosubj@gmail.com>
2026-05-08 16:49:41 +0100

    #

--- src/main/java/com/semantyca/core/model/cnst/ArchivedStatus.java
+++ src/main/java/com/semantyca/core/model/cnst/ArchivedStatus.java
@@ -3,19 +3,20 @@ package com.semantyca.core.model.cnst;
 import lombok.Getter;
 
 @Getter
-public enum LifecycleStatus {
-    NOT_APPROVED(10),
-    APPROVED(12),
-    REJECTED(13);
+public enum ArchivedStatus {
+    VALID(0),
+    ARCHIVED(1),
+    HIDDEN(10);
+
 
     private final int code;
 
-    LifecycleStatus(int code) {
+    ArchivedStatus(int code) {
         this.code = code;
     }
 
-    public static LifecycleStatus fromCode(int code) {
-        for (LifecycleStatus s : values()) {
+    public static ArchivedStatus fromCode(int code) {
+        for (ArchivedStatus s : values()) {
             if (s.code == code) return s;
         }
         throw new IllegalArgumentException("Unknown code: " + code);
