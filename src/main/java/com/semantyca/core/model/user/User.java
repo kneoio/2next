b58ec3cd43e0c244aa73153cf8f8304ecf0cb986
aidazi <biosubj@gmail.com>
2026-05-12 17:25:09 +0100

    #

--- src/main/java/com/semantyca/core/model/user/User.java
+++ src/main/java/com/semantyca/core/model/user/User.java
@@ -1,18 +1,15 @@
 package com.semantyca.core.model.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.semantyca.core.model.cnst.LanguageCode;
 import com.semantyca.core.model.DataEntity;
 import com.semantyca.core.model.Module;
 import com.semantyca.core.repository.cnst.UserRegStatus;
-import com.semantyca.core.server.EnvConst;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -28,7 +25,6 @@ public class User extends DataEntity<Long> implements IUser {
     private boolean isSupervisor;
     private List<Module> modules = new ArrayList<>();
     private List<Role> roles = new ArrayList<>();
-    private Integer pageSize = EnvConst.DEFAULT_PAGE_SIZE;
     private Integer defaultLang;
     private TimeZone timeZone;
     private UserRegStatus regStatus;
--- src/main/java/com/semantyca/mixpla/model/brand/Owner.java
+++ src/main/java/com/semantyca/mixpla/model/brand/Owner.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Owner {
+    private int userId;
     private String name;
     private String email;
 }
