ed6ace5e7149ed8b554ea3f0e7015ce0329ba96f
aidazi <biosubj@gmail.com>
2026-05-19 12:40:11 +0100

    Fix read-side LocalDateTime to OffsetDateTime in UserSubscription, UserConsent, DocumentAccessInfo

--- src/main/java/com/semantyca/core/model/embedded/DocumentAccessInfo.java
+++ src/main/java/com/semantyca/core/model/embedded/DocumentAccessInfo.java
@@ -3,13 +3,13 @@ package com.semantyca.core.model.embedded;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Setter
 @Getter
 public class DocumentAccessInfo {
     private Long userId;
-    private LocalDateTime readingTime;
+    private OffsetDateTime readingTime;
     private Boolean canEdit;
     private Boolean canDelete;
     private String userLogin;
