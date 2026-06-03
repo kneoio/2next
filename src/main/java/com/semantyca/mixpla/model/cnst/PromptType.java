f69f3de1c8dc7a6d6dc4865f3bc4c688ec8ae745
aidazi <biosubj@gmail.com>
2026-06-03 20:41:14 +0100

    #

--- src/main/java/com/semantyca/mixpla/model/cnst/PromptType.java
+++ src/main/java/com/semantyca/mixpla/model/cnst/PromptType.java
@@ -1,5 +1,5 @@
 package com.semantyca.mixpla.model.cnst;
 
 public enum PromptType {
-    SONG_INTRO, ADVERTISEMENT_INTRO, REMINDER_INTRO, GENERATOR
+    SONG_INTRO, ADVERTISEMENT_INTRO, PODCAST_INTRO, REMINDER_INTRO, GENERATOR
 }
