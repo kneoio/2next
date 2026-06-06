57634f11a570ce5f670f665f0a00c404aec55b8c
aidazi <biosubj@gmail.com>
2026-06-06 17:50:21 +0100

    #

--- src/main/java/com/semantyca/mixpla/model/brand/Owner.java
+++ src/main/java/com/semantyca/mixpla/model/brand/Owner.java
@@ -3,12 +3,15 @@ package com.semantyca.mixpla.model.brand;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Owner {
-    private Long userID;
+    private Long userId;
     private String name;
     private String email;
     private boolean exposeWhileSharing;
     private boolean actionDebugEnabled;
+    private List<Owner> coOwners;
 }
