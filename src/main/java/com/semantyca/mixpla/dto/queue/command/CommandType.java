cbf2d8d2cc85991d006fa599bca80c9f54a5a07d
aidazi <biosubj@gmail.com>
2026-06-16 17:55:51 +0100

    #

--- src/main/java/com/semantyca/mixpla/dto/queue/command/CommandType.java
+++ src/main/java/com/semantyca/mixpla/dto/queue/command/CommandType.java
@@ -1,5 +1,10 @@
 package com.semantyca.mixpla.dto.queue.command;
 
 public enum CommandType {
-    FLOW_RESTART
+    FLOW_RESTART,
+    SONG_RATED,
+    WAKE_UP_DJ,
+    GET_SLEEP_DJ,
+    REBUILD_AGENDA,
+    BACK_PRESSURE_EMISSION
 }
