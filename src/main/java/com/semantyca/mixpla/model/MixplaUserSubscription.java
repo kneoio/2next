37da2c63de9b8563b146b12129fa28f44012e846
aidazi <biosubj@gmail.com>
2026-06-20 18:01:58 +0100

    feat: change djTypeId (UUID) to djType (List<String>) in MixplaUserSubscription

--- src/main/java/com/semantyca/mixpla/model/MixplaUserSubscription.java
+++ src/main/java/com/semantyca/mixpla/model/MixplaUserSubscription.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.UUID;
 
 @Setter
 @Getter
@@ -20,7 +19,7 @@ public class MixplaUserSubscription extends UserSubscription {
     private boolean otsAllowed;
     private Integer maxSongs;
     private Integer streamQualityKbps;
-    private UUID djTypeId;
+    private List<String> djType;
     private short supportLevel;
     private boolean customScriptAllowed;
     private Integer maxStations;
