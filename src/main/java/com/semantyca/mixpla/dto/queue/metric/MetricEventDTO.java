24757a261e76c30fab80cad1f18b9e968b8420b4
kaira <justaidajam@gmail.com>
2026-06-20 11:25:03 +0100

    #

--- src/main/java/com/semantyca/mixpla/dto/queue/metric/MetricEventDTO.java
+++ src/main/java/com/semantyca/mixpla/dto/queue/metric/MetricEventDTO.java
@@ -1,6 +1,7 @@
 package com.semantyca.mixpla.dto.queue.metric;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -11,10 +12,53 @@ public record MetricEventDTO(
         ProcessType processType,
         Instant timestamp,
         UUID traceId,
+        List<UUID> parentTraces,
         String code,
         Map<String, Object> payload
 ) {
-    public static MetricEventDTO of(String serviceId, String brandName, MetricEventType type, ProcessType processType, UUID traceId, String code, Map<String, Object> payload) {
-        return new MetricEventDTO(serviceId, brandName, type, processType, Instant.now(), traceId, code, payload);
+    public static MetricEventDTO of(
+            String serviceId,
+            String brandName,
+            MetricEventType type,
+            ProcessType processType,
+            UUID traceId,
+            List<UUID> parentTraces,
+            String code,
+            Map<String, Object> payload
+    ) {
+        return new MetricEventDTO(
+                serviceId,
+                brandName,
+                type,
+                processType,
+                Instant.now(),
+                traceId,
+                parentTraces,
+                code,
+                payload
+        );
+    }
+
+    @Deprecated
+    public static MetricEventDTO of(
+            String serviceId,
+            String brandName,
+            MetricEventType type,
+            ProcessType processType,
+            UUID traceId,
+            String code,
+            Map<String, Object> payload
+    ) {
+        return new MetricEventDTO(
+                serviceId,
+                brandName,
+                type,
+                processType,
+                Instant.now(),
+                traceId,
+                List.of(),
+                code,
+                payload
+        );
     }
 }
