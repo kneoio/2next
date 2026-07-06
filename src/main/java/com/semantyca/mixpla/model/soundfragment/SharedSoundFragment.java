e7010dfb3f0aab09b6bfb1b8b6bf71935410de59
aidazi <biosubj@gmail.com>
2026-07-06 18:30:18 +0100

    #

--- src/main/java/com/semantyca/mixpla/model/soundfragment/SharedSoundFragment.java
+++ src/main/java/com/semantyca/mixpla/model/soundfragment/SharedSoundFragment.java
@@ -1,5 +1,6 @@
 package com.semantyca.mixpla.model.soundfragment;
 
+import com.semantyca.core.model.FileMetadata;
 import com.semantyca.core.model.SecureDataEntity;
 import com.semantyca.core.model.cnst.LanguageCode;
 import com.semantyca.mixpla.model.cnst.PlaylistItemType;
@@ -14,15 +15,20 @@ import java.util.UUID;
 @Getter
 @Setter
 public class SharedSoundFragment  extends SecureDataEntity<UUID> {
-    private long sourceUserId;
+    private UUID id;
+    private OffsetDateTime regDate;
+    private OffsetDateTime lastModDate;
+    private Long sourceUserId;
     private String sourceUserName;
     private String sourceUserEmail;
     private UUID targetBrandId;
     private UUID soundFragmentId;
     private OffsetDateTime expiresAt;
-    private int playedCount;
-    private int ratedCount;
-    private int status;
+    private Integer playedCount;
+    private Integer ratedCount;
+    private Integer boost;
+    private Integer status;
+    private Integer archived;
     private String brandSlugName;
     private String title;
     private String artist;
@@ -31,4 +37,5 @@ public class SharedSoundFragment  extends SecureDataEntity<UUID> {
     private List<UUID> genres;
     private List<UUID> labels;
     private EnumMap<LanguageCode, String> targetBrandName;
+    private List<FileMetadata> fileMetadataList;
 }
